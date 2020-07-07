c0a16c4d011d7caa6edd0ef26038c646977772c7
sodel <cata.sandu11@gmail.com>
2020-07-07 15:27:38 +0300

    salutw

---
+++ Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaje.Afiseaza("Salut lume!", "EN");
+            Mesaje.Afiseaza("Salut lume!", "RO");
+            Mesaje.Afiseaza("Salut lume!", "HU");
+            Mesaje.Afiseaza("Salut lume!", "ES");
+            Mesaje.Afiseaza("Sanatate!");
+        }
+    }
+}
